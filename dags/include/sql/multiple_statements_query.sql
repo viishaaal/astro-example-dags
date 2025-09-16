588a23562868f9e6793531ac8105e1b7e19de572
viishaaal <71539297+viishaaal@users.noreply.github.com>
2025-09-16 12:36:49 -0400

    Create multiple_statements_query.sql

---
+++ dags/include/sql/multiple_statements_query.sql
@@ -0,0 +1,9 @@
+INSERT INTO {{ params.db_name }}.{{ params.schema_name }}.{{ params.table_name }} (ID, NAME)
+VALUES
+    (2, 'Peanut'),
+    (3, 'Butter');
+
+INSERT INTO {{ params.db_name }}.{{ params.schema_name }}.{{ params.table_name }} (ID, NAME)
+VALUES
+    (4, 'Vega'),
+    (5, 'Harper');
