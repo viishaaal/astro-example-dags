a1391c5eb3b79653c333cca12ec135f00b9377a0
viishaaal <71539297+viishaaal@users.noreply.github.com>
2025-09-16 16:35:46 -0400

    Update my_snowflake_dag.py

--- dags/my_snowflake_dag.py
+++ dags/my_snowflake_dag.py
@@ -13,7 +13,7 @@ from airflow.models.baseoperator import chain
 from pendulum import datetime, duration
 import os
 
-_SNOWFLAKE_CONN_ID = "Krishna"
+_SNOWFLAKE_CONN_ID = "KRISHNA"
 _SNOWFLAKE_DB = "KRISHNA_BRONZE"
 _SNOWFLAKE_SCHEMA = "RAW_STG"
 _SNOWFLAKE_TABLE = "DEMO_TABLE"
@@ -97,3 +97,4 @@ def my_snowflake_dag():
 
 
 my_snowflake_dag()
+
