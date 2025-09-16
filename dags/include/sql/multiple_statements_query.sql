b41297d45e293fbc632918dfb5688a0de7d06a5d
viishaaal <71539297+viishaaal@users.noreply.github.com>
2025-09-16 14:10:17 -0400

    Update multiple_statements_query.sql

--- dags/include/sql/multiple_statements_query.sql
+++ dags/include/sql/multiple_statements_query.sql
@@ -1,9 +1,1 @@
-INSERT INTO {{ params.db_name }}.{{ params.schema_name }}.{{ params.table_name }} (ID, NAME)
-VALUES
-    (2, 'Peanut'),
-    (3, 'Butter');
 
-INSERT INTO {{ params.db_name }}.{{ params.schema_name }}.{{ params.table_name }} (ID, NAME)
-VALUES
-    (4, 'Vega'),
-    (5, 'Harper');
